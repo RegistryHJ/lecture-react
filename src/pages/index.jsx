6ddcf681a96193a5a40751bcf4cb9b5db0ed7e74
RegistryHJ <registry.fordev@gmail.com>
2025-08-22 20:52:03 +0900

    Initial Commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export default function IndexPage() {
+  return (
+    <div className='index-page'>
+      <h1>Welcome to the Index Page</h1>
+      <p>This is the main entry point of our application.</p>
+    </div>
+  );
+}
