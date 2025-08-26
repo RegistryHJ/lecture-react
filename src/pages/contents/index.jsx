154aee6cc380963953bf91c1ea92b36973f5b162
RegistryHJ <registry.fordev@gmail.com>
2025-08-26 21:29:32 +0900

    feat: add ContentsPage (/contents)

---
+++ src/pages/contents/index.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export default function ContentsPage() {
+  return (
+    <div className='contents-page'>
+      <h1>Welcome to the Contents Page</h1>
+      <p>This is the contents entry point of our application.</p>
+    </div>
+  );
+}
