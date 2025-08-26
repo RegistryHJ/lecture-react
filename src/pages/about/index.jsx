df56446510873178b2460a40bba8e6acbaf1d400
RegistryHJ <registry.fordev@gmail.com>
2025-08-26 21:29:14 +0900

    feat: add AboutPage (/about)

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export default function AboutPage() {
+  return (
+    <div className='about-page'>
+      <h1>Welcome to the About Page</h1>
+      <p>This is the about entry point of our application.</p>
+    </div>
+  );
+}
