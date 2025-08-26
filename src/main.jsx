938c6fcd8bed479bd9feb9bfceb187d37f655bc0
RegistryHJ <registry.fordev@gmail.com>
2025-08-26 21:38:21 +0900

    feat: add router with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
 import './main.css';
 import IndexPage from './pages';
+import AboutPage from './pages/about';
+import ContentsPage from './pages/contents';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <IndexPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<IndexPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contents' element={<ContentsPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
